0ba612f2a8b697b7cb9f54155e5991221f4e140d
IvanAjdu <ivan.ajdu@gmail.com>
2024-02-15 20:56:07 +0100

    Texture for sidewalk; Random spawn timer with delay; easy 20 seconds then hard.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
 
     public GameObject obstaclePrefab;
-    private Vector3 spawnPosition = new Vector3(30,0,0);
-    private float spawnDelay = 2f;
-    private float repeatRate = 2f;
+    private Vector3 spawnPosition = new Vector3(30, 0, 0);
+    //private float spawnDelay = 2f;
+    //private float repeatRate = 1f;
     private PlayerController playerControllerScript;
 
 
@@ -16,20 +17,35 @@ public class SpawnManager : MonoBehaviour
     void Start()
     {
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
-        InvokeRepeating("spawnObstacle", spawnDelay, repeatRate);
+        randomSpawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
+    void randomSpawn()
+    {
+        float randomSpawnObstacle;
+        if (Time.time < 20)
+        {
+            randomSpawnObstacle = Random.Range(1.5f, 3f);
+        }
+        else
+        {
+            randomSpawnObstacle = Random.Range(0.9f, 1.9f);
+        }
+        Invoke("spawnObstacle", randomSpawnObstacle);
+    }
+  
     void spawnObstacle()
     {
         if (playerControllerScript.gameOver == false)
         {
             Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+            randomSpawn();
         }
     }
 }
